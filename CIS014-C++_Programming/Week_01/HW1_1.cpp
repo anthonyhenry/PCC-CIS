23e68223bf0d949017fa8130ffa386419a1b4a78
anthonyhenry <anthonyh1497@gmail.com>
2023-09-10 22:26:22 -0700

    HW_1 Part 2
    Updated the program to convert USD to CAD

--- CIS014-C++_Programming/Week_01/HW1_1.cpp
+++ CIS014-C++_Programming/Week_01/HW1_1.cpp
@@ -14,32 +14,44 @@ int main() {
     cout << "Please enter all of your pennies: " << endl;
     cin >> numPennies;
 
-    // YOUR CODE HERE
-    cout << "In US currency you have: ";
-
+    //Find out how many dollars there are
     int dollars = numPennies / 100;
+    cout << "In US currency you have: ";
     cout << dollars << " dollars, ";
 
-    int remainingPennies = numPennies % 100;
-    // cout << endl << "Remaining pennies:" << remainingPennies << endl;
+    //Find out how many remaining cents there are    
+    float cents = (numPennies - (dollars * 100)) * 0.01;
+    //Save the total amount of money in the format $##.## for use later in converting Canadian currency
+    float money = dollars + cents;
 
+    //Find remaining pennies
+    int remainingPennies = numPennies % 100;
+
+    //Find out how many quarters there are
     int quarters = remainingPennies / 25;
     cout << quarters << " quarters, ";
 
+    //Find remaining pennies
     remainingPennies -= 25 * quarters;
-    // cout << endl << "Remaining pennies: " << remainingPennies << endl;
-
+    
+    //Find out how many dimes there are
     int dimes = remainingPennies / 10;
     cout << dimes << " dimes, ";
 
+    //Find remaining pennies
     remainingPennies -= 10 * dimes;
-    // cout << endl << "Remaining pennies " << remainingPennies << endl;
-
+    
+    //Find out how many nickels there are
     int nickels = remainingPennies / 5;
     cout << nickels << " nickels, ";
 
+    //Find last remaining pennies
     remainingPennies -= 5 * nickels;
     cout << "and " << remainingPennies << " pennies." << endl;
+
+    //Convert pennies to Canadian dollars
+    float CAD = money * 1.33;
+    cout << "In Canadian currency you have: " << CAD << " dollars.";
 
     return 0;
 }
