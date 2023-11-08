25aa5f70f884c0620706cff9db8a24370236f58b
anthonyhenry <anthonyh1497@gmail.com>
2023-11-07 21:09:47 -0800

    9_1 Suitable for submitting

--- CIS014-C++_Programming/Week_10/HW9_1.cpp
+++ CIS014-C++_Programming/Week_10/HW9_1.cpp
@@ -20,114 +20,57 @@ public:
  * RETURN VALUES:
  */
 int CIS14::getMaxPoints(vector<vector<int>> &maze) {
-    int n = maze.size();
-    if (n == 0) {
+    if (maze.size() == 0) {
         return 0; // Empty input, no points can be earned
     }
 
-    // Iterate from the top row to the second-to-last row
-    for (int row = 1; row < n; row++) {
-        int m = maze[row].size();
-        for (int col = 0; col < m; col++) {
-            // Update the current cell with the maximum points by choosing the best path
-            if (col == 0) {
+    //Start at row 2 (maze[1]) and work down the pyramid comparing with the numbers above to see which results in a higher number
+    for(int row = 1; row < maze.size(); row++)
+    {
+        for(int col = 0; col < maze[row].size(); col++)
+        {
+            cout << "Previous: " << maze[row][col] << " | ";
+
+            // For the first column, can only add with the number in the column immediately above 
+            if(col == 0)
+            {
                 maze[row][col] += maze[row - 1][col];
-            } else if (col == m - 1) {
-                maze[row][col] += maze[row - 1][col - 1];
-            } else {
-                maze[row][col] += max(maze[row - 1][col - 1], maze[row - 1][col]);
             }
+            // For the last column in a row, can only add with the number in the previous column of the row above
+            else if (col == maze[row].size() - 1)
+            {
+                maze[row][col] += maze[row - 1][col - 1];
+            }
+            // For all other columns, need to check which previous number is larger and add with that number
+            else
+            {
+                // Conditional operator to only add the larger number
+                maze[row][col] += (maze[row - 1][col] > maze[row - 1][col - 1]) ? maze[row - 1][col] : maze[row - 1][col - 1];
+
+                // This does the same as above but in more lines of code
+                // if(maze[row - 1][col] > maze[row - 1][col - 1])
+                // {
+                //     maze[row][col] += maze[row - 1][col];
+                // }
+                // else
+                // {
+                //     maze[row][col] += maze[row - 1][col - 1];
+                // }
+            }
+            cout << "Row: " << row << " | Col: " << col << " | New: " << maze[row][col] << endl;
         }
     }
 
     // Find the maximum points in the last row (bottom row)
-    int maxPoints = maze[n - 1][0];
-    for (int col = 1; col < maze[n - 1].size(); col++) {
-        maxPoints = max(maxPoints, maze[n - 1][col]);
+    int maxPoints = maze[maze.size() - 1][0];
+
+    cout << "maxPoints" << endl;
+
+    for (int col = 1; col < maze[maze.size() - 1].size(); col++) {
+        maxPoints = max(maxPoints, maze[maze.size() - 1][col]);
     }
 
     return maxPoints;
-
-
-
-    // int maxPoints = 0;
-
-    // // cout << "Row 1 size: " << maze[1].size() << endl;
-
-
-
-
-
-
-
-
-    // // int rows = maze.size();
-    // // int cols = 1;
-    // // int placeToBranch = maze.size() - 1;
-
-    // // int X = 0;
-    // // int Y = 0;
-    // // int xToBranchAt = maze.size();
-
-    // // while()
-
-    // // while(Y < maze.size())
-    // // {
-    // //     while(X < maze.size())
-    // //     {
-    // //         cout << "(" << X << ", " << Y << "): ";
-    // //         cout << maze[X][Y] << endl;
-    // //         X++;
-    // //     }
-    // //     cout << "---" << endl;
-    // //     X = 0;
-    // //     Y++;
-    // // }
-
-    // int x = 0;
-    // int y = 0;
-    // // int startYChange = 3;
-
-    // int beginYChangeAt = maze.size();
-    // int numTimesToChangeY = 0;
-
-    // int iterator = 0;
-
-    // while(iterator < 8/*x != maze.size() && y != maze.size()*/)
-    // {
-    //     while(x < maze.size())
-    //     {
-    //         cout << "(" << x << ", " << y << "): " << maze[x][y] << endl;
-    //         if(x == beginYChangeAt)
-    //         {
-    //             y++;
-    //         }
-    //         x++;
-    //     }
-    //     x=0;
-    //     beginYChangeAt--;
-    //     cout << "---" << endl;
-
-    //     iterator++;
-    // }
-
-
-    // // cout << "---" << endl;
-
-    // cout << maze[0][0] << maze[1][0] << maze[2][0] << maze[3][0] << endl;
-
-    // cout << maze[0][0] << maze[1][0] << maze[2][0] << maze[3][1] << endl;
-    // cout << maze[0][0] << maze[1][0] << maze[2][1] << maze[3][1] << endl;
-    // cout << maze[0][0] << maze[1][1] << maze[2][1] << maze[3][1] << endl;
-    
-
-    // cout << maze[0][0] << maze[1][0] << maze[2][1] << maze[3][2] << endl;
-    // cout << maze[0][0] << maze[1][1] << maze[2][1] << maze[3][2] << endl;
-    
-    // cout << maze[0][0] << maze[1][1] << maze[2][2] << maze[3][2] << endl;
-    // cout << maze[0][0] << maze[1][1] << maze[2][2] << maze[3][3] << endl;
-
-    // return maxPoints;
 }
 
 int main()
@@ -137,7 +80,10 @@ int main()
     
     // Your target method will be tested as such, with random input
     // as the following example illustrates
-    vector<vector<int>> maze = {{0}, {4,1}, {5,3,8}, {1,6,7,3}};
+    vector<vector<int>> /*maze = {{0}, {4,1}, {5,3,8}, {1,6,7,3}};*/maze = {{1}, {1,22305}, {1,29394,1}, {1,1,1234,1}, {1,1,3,1,10}}; // output = 52937
+
+
+
     cout << cis14.getMaxPoints(maze) << endl; // 16
     
     return 0;
