ebefb110811fbde51b288fdf3d0a1551a64bf765
anthonyhenry <anthonyh1497@gmail.com>
2023-11-12 15:54:32 -0800

    Update HW10_1.cpp
    Added comments

--- CIS014-C++_Programming/Week_11/HW10_1.cpp
+++ CIS014-C++_Programming/Week_11/HW10_1.cpp
@@ -22,26 +22,30 @@ vector<int> CIS14::getMaxHits(vector<int> &requests, int window_size) {
     // Create an empty vector to hold max values
     vector<int> maxHits;
 
+    // If the window size is 0, or larger than the size of the requests vector, return an empty vector
     if(window_size == 0 || window_size > requests.size())
     {
         return maxHits;
     }
 
-
+    // Loop through the requests vector, stop at the length of the requests vector minus the window size parameter value
     for(int i = 0; i <= requests.size() - window_size; i++)
     {
+        // Temporary variable for storing the local max
         int max = requests.at(i);
         cout << max;
+        // Loop through the next values in the requests vector as specified by the window size argument
         for(int j = 1; j < window_size; j++)
         {
             cout << ", " << requests.at(i+j);
-
+            // Update the local max variable if a new local max has been found
             if(requests.at(i+j) > max)
             {
                 max = requests.at(i+j);
             }
         }
         cout << " | Max = " << max << endl;
+        // Add the max variable to the maxHits vector
         maxHits.push_back(max);
     }
 
