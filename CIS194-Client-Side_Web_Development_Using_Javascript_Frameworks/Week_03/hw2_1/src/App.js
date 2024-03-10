6d287105e1833590b8e0c54850c9e71e38800275
anthonyhenry <anthonyh1497@gmail.com>
2024-03-09 17:21:45 -0800

    Final version of App.js includes both methods

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_03/hw2_1/src/App.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_03/hw2_1/src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { createElement } from 'react';
+
 function App() {
   const react = ['NPM', 'Node.js', 'JXS', 'Babel', 'Transpiler', 'Props', 'Single Page Application'];
   
   return (
+    // These two bits of code do the exact same thing.
+
+    // This one uses JSX
     <ol>
       {react.map((listItem) => (
         <li>{listItem}</li>
       ))}
     </ol>
+    
+    // This one uses React.createElement
+    // <ol>
+    //   {react.map((listItem) => (
+    //     createElement(
+    //       'li',
+    //       null,
+    //       listItem
+    //     )
+    //   ))}
+    // </ol>
   );
 }
+
 export default App;
