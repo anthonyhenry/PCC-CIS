65482e6f075c6573d1295bda73a5e4e75d98c556
anthonyhenry <anthonyh1497@gmail.com>
2024-03-16 15:30:57 -0700

    Creating your own react component

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_04/react-notes/src/App.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_04/react-notes/src/App.js
@@ -13,20 +13,35 @@ const projects = [
   }
 ];
 
+// Each function below is a React component
+
+// The App component is the entry point and responsible for rendering stuff
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ol>
-          {/* Iterate through projects array to show projects as link elements */}
-          {projects.map(function(project){
-            return <li><a href={project.link} target="_blank">{project.name}</a></li>
-          })}
+          {/* This is how you call a component*/}
+          <List_Projects/> 
         </ol>
       </header>
     </div>
   );
+}
+
+// Create a component to display items from the project array
+function List_Projects()
+{
+  // Iterate through projects array to show projects as link elements
+  return projects.map(project => // Arrow function
+    <li><a href={project.link} target="_blank">{project.name}</a></li>
+  )
+  
+  // This is how you do the above without an arrow function
+  // return projects.map(function(project){
+  //   return <li><a href={project.link} target="_blank">{project.name}</a></li>
+  // })
 }
 
 export default App;
