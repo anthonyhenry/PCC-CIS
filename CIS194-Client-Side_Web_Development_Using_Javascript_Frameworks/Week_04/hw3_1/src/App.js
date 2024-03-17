0ce8918aacd730a3d45454ddcef46ac6a1856539
anthonyhenry <anthonyh1497@gmail.com>
2024-03-16 18:10:47 -0700

    Assignment finished

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_04/hw3_1/src/App.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_04/hw3_1/src/App.js
@@ -5,7 +5,11 @@ import Card from "./Card"
 function App() {
  
     // YOUR CODE HERE
-    
+    return (
+        data.map(function(fruit){
+            return <Card img={fruit.coverImg} title={fruit.title} cost={fruit.price} kingdom={fruit.stats.kingdom} family={fruit.stats.family} genus={fruit.stats.genus} origin={fruit.location}/>
+        })
+    );
 }
 
 export default App;
--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_04/hw3_1/src/Card.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_04/hw3_1/src/Card.js
@@ -1,5 +1,16 @@
 import React from "react"
 
 export default function Card(props) {
-  // YOUR CODE HERE
+  const imgSrc = "images/" + props.img;
+  return (
+    <div>
+        <img src={imgSrc}/>
+        <p>{props.title}</p>
+        <p><strong>Average cost ${props.cost}</strong> / LB</p>
+        <p>Kingdom: {props.kingdom}</p>
+        <p>Family: {props.family}</p>
+        <p>Genus: {props.genus}</p>
+        <p>Origin: {props.origin}</p>
+    </div>
+  );
 }
