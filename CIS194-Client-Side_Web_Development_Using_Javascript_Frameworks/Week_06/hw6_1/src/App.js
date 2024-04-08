28e20b17ceff326bed32359fec3f1df35765aaeb
anthonyhenry <anthonyh1497@gmail.com>
2024-04-07 23:45:48 -0700

    Solution with visual output results

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_06/hw6_1/src/App.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_06/hw6_1/src/App.js
@@ -2,28 +2,79 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-const useMyCustomHook = (key, initialState) => {
-  // State Hook
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-
-  React.useEffect(
-    ()=> {
-      localStorage.setItem(key, value);
-    },
-    [key, value]
-  );
-
-  return [value, setValue]
+class Sentence {
+  constructor(word1, word2) {
+    this.word1 = word1;
+    this.word2 = word2;
+  }
+  getSentence() {
+    return this.word1 + ' ' + this.word2;
+  }
 }
 
+const useMyCustomHook = (key, initialState) => {
+   // State Hook
+   const [value, setValue] = React.useState(
+      localStorage.getItem(key) || initialState
+   );
+
+   // Side Effect Hook
+   React.useEffect(
+      ()=> {
+         localStorage.setItem(key, value);
+      }, 
+      [key, value]
+   );
+
+   // Return array for destructuring
+   return [value, setValue];
+};
+
 const App = () => {
-  // Your "key" is 'React'
-  // Your "initialState" is 'Redux'
+  const hello_world = new Sentence('Hello', 'World!');
+  const react_rocks = new Sentence('React', 'rocks!');
+
   const [checkTerm, setCheckTerm] = useMyCustomHook(
-    'React',
+    'React', 
     'Redux'
+  );
+
+  const handleInput = evt => {
+    setCheckTerm(evt.target.value);
+    console.log(evt.target.value);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* creating/instantiating an instance of List component */}
+          <List first={hello_world} second={react_rocks} />
+        </p>
+        <p>
+          <Input onInput={handleInput} userInput={checkTerm} />
+        </p>
+      </header>
+    </div>
+  );
+};
+
+const Input = ({ onInput, userInput }) => (
+  <> 
+    <label htmlFor="Check">Check: </label>
+    <input id="check" type="text" value={userInput} onChange={onInput} />
+    <p>Checking for <b>{userInput}</b></p>
+  </>
+)
+
+const List = props => {
+  return (
+    <div>
+      {props.first.getSentence()}
+      <hr />
+      {props.second.getSentence()}
+    </div>
   );
 };
 
