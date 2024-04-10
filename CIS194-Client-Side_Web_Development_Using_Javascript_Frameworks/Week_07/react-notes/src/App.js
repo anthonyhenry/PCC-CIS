e92720f05dbbf1f317166a73e0fc695896ea88e2
anthonyhenry <anthonyh1497@gmail.com>
2024-04-09 19:04:34 -0700

    Notes

---
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_07/react-notes/src/App.js
@@ -0,0 +1,88 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class Sentence {
+  constructor(word1, word2) {
+    this.word1 = word1;
+    this.word2 = word2;
+  }
+
+  getSentence() {
+    return this.word1 + ' ' + this.word2;
+  }
+}
+
+// function declaration 
+// function name() {...}
+
+// arrow function: 
+// const name() => { ... } 
+
+
+const App = () => {
+
+  const handleInput = evt => {
+    setCheckTerm(evt.target.value);
+    console.log(evt.target.value);
+  };
+
+  const hello_world = new Sentence('Hello', 'World!');
+  const react_rocks = new Sentence('React', 'rocks!');
+
+  const [checkTerm, setCheckTerm] = React.useState(
+    localStorage.getItem("input") || "nothing"
+  );
+
+  React.useEffect(
+    () => {
+      localStorage.setItem("input", checkTerm);
+    },
+    [checkTerm]
+  );
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* creating/instantiating an instance of List component */}
+          <List first={hello_world} second={react_rocks} />
+        </p>
+        <p>
+          <Input
+          id="check"
+          value="Checking for "
+          onInput={handleInput}
+          userInput={checkTerm}
+          >
+            {/* Anything here is a child of the Input component */}
+            <b>Check: </b>
+          </Input>
+        </p>
+      </header>
+    </div>
+  );
+
+};
+
+const Input = ({ id, value, type = 'text', onInput, userInput, children }) => (
+  <>
+    <label htmlFor={id}>{children} </label> {/* children here refers to the html in between the opening and closing tag for this componnent */}
+    <input id={id} type={type} value={userInput} onChange={onInput} />
+    <p>{value} <b>{userInput}</b></p>
+  </>
+);
+
+const List = props => {
+  return (
+    <div>
+      {props.first.getSentence()}
+      <hr />
+      {props.second.getSentence()}
+    </div>
+  );
+};
+
+export default App;
