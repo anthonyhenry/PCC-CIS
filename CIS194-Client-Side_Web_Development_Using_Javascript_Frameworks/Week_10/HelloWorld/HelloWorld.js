c627f0487809ac5a9234c11ac22f408a54608606
anthonyhenry <anthonyh1497@gmail.com>
2024-05-04 17:43:20 -0700

    notes

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_10/HelloWorld/HelloWorld.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_10/HelloWorld/HelloWorld.js
@@ -1,2 +1,43 @@
+// 
+// String
 var message = 'Hello World';
 console.log(message);
+// Number
+var i = 10;
+console.log(i);
+// Boolean
+var fact = true;
+console.log(fact);
+// Any
+// Any is a special type that you can use whenver you don't want a particular value to cause typechecking errors
+var obj = {
+    x: 0
+};
+// Assigning types to function parameters
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+var me = "Anthony";
+greet(me);
+// Specifying function return types
+function getFavoriteNumber() {
+    return 26;
+}
+// null and undefined
+// If you need a function to check for a specific type or null, you can use |
+function doSomething(x) {
+    if (x === null) {
+        // do something
+    }
+    else {
+        console.log("Hello there, " + x);
+    }
+}
+function printPoint(pt) {
+    console.log("(" + pt.x + ", " + pt.y + ")");
+}
+printPoint({ x: 100, y: 100 });
+function printCord(cd) {
+    console.log(cd.x + ", " + cd.y);
+}
+printCord({ x: 10, y: 10 });
--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_10/HelloWorld/HelloWorld.ts
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_10/HelloWorld/HelloWorld.ts
@@ -1,2 +1,82 @@
+// 
+
+// String
 let message: string = 'Hello World';
 console.log(message);
+
+// Number
+let i: number = 10;
+console.log(i);
+
+// Boolean
+let fact: boolean = true;
+console.log(fact);
+
+// Any
+    // Any is a special type that you can use whenver you don't want a particular value to cause typechecking errors
+let obj: any = {
+    x: 0
+};
+
+// Assigning types to function parameters
+function greet(name: string)
+{
+    console.log("Hello, " + name + "!");
+}
+
+let me: string = "Anthony";
+
+greet(me);
+
+// Specifying function return types
+function getFavoriteNumber(): number
+{
+    return 26;
+}
+
+// null and undefined
+    // If you need a function to check for a specific type or null, you can use |
+function doSomething(x: string | null)
+{
+    if(x === null)
+    {
+        // do something
+    }
+    else
+    {
+        console.log("Hello there, " + x);
+    }
+}
+
+// Type assertions
+    // You can use type assertion to specify a more specific type
+// const myCanvas = document.getElementById("") as HTMLCanvasElement;
+
+// Type aliases
+    // When you want to use the same type more than once and refer to it by a single name you can create a type alias
+type Point = {
+    x: number;
+    y: number;
+};
+
+function printPoint(pt: Point)
+{
+    console.log("(" + pt.x + ", " + pt.y + ")");
+}
+
+printPoint({x: 100, y: 100});
+
+// Interfaces
+    // An interface is another way to name an object type
+    // The difference between interfaces and type aliaces is that interfaces can be re-opened to add new properties to them
+interface Cord {
+    x: number;
+    y: number;
+}
+
+function printCord(cd: Cord)
+{
+    console.log(cd.x + ", " + cd.y);
+}
+
+printCord({ x: 10, y: 10});
