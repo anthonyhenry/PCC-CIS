6444d3e0f26cbed8ac4aeb7bcb983dbd5bd9619b
anthonyhenry <anthonyh1497@gmail.com>
2024-05-19 19:12:59 -0700

    setup business card

---
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_11/dark-mode-demo/src/BusinessCard.js
@@ -0,0 +1,31 @@
+function Name(props)
+{
+    return <h1>{props.name}</h1>
+}
+
+function Title(props)
+{
+    return <p>{props.title}</p>
+}
+
+function Email(props)
+{
+    return <p>{props.email}</p>
+}
+
+function Phone(props)
+{
+    return <p>{props.phone}</p>
+}
+
+export default function BusinessCard()
+{
+    return(
+        <div className="light">
+            <Name name="Anthony"/>
+            <Title title="Game Designer"/>
+            <Email email="anthonyh1497@gmail.com"/>
+            <Phone phone="805-232-6299"/>
+        </div>
+    )
+}
