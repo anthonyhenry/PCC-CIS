8eaf13810ca3c3e6d2c61e8a525825b3e919321d
anthonyhenry <anthonyh1497@gmail.com>
2024-05-23 22:30:47 -0700

    login demo part 1

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/App/index.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/App/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Login from '../Login'
+import LandingPage from '../Landing'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Navigation from '../Navigation';
+import * as ROUTES from "../constants/paths"
 
 function App()
 {
     return(
         <>
             <h1>App</h1>
+            <BrowserRouter>
+                <Navigation/>
+                <Routes>
+                    <Route path={ROUTES.LOGIN} element={<Login/>} />
+                    <Route path={ROUTES.LANDING} element={<LandingPage/>} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
