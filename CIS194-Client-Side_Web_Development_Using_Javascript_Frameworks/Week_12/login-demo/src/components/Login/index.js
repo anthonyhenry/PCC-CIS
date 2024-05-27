a755f304f4150c394e7c7cb19fc9247f4a05863b
anthonyhenry <anthonyh1497@gmail.com>
2024-05-27 13:53:33 -0700

    finished notes

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/Login/index.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/Login/index.js
@@ -1,99 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-async function fetchToken(creds) {
-  return fetch("http://localhost:8080/login", {
-    method: "POST",
-    header: {
-      "Content-Type": "applicatioin/json",
-    },
-    body: JSON.stringify(creds),
-  }).then((data) => data.json());
+async function fetchToken(creds) 
+{
+    return fetch("http://localhost:8080/login", {
+        method: "POST",
+        header: {
+        "Content-Type": "applicatioin/json",
+        },
+        body: JSON.stringify(creds),
+    }).then((data) => data.json());
 }
 
-function Login({ setToken }) {
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
+function Login( {setToken} )
+{
+    const [username, setUserName] =useState();
+    const [password, setPassword] = useState();
 
-  const handleSubmit = async (evt) => {
-    evt.preventDefault();
-    const token = await fetchToken({
-      username,
-      password
-    });
-    setToken(token);
-  };
+    const handleSubmit = async evt => {
+        evt.preventDefault();
+        const token = await fetchToken({
+            username, 
+            password,
+        });
+        setToken(token);
+    }
 
-return (
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input
-            type="text"
-            onChange={(evt) => setUserName(evt.target.value)}
-          />
-        </label>
-        <label>
-          <p>Password</p>
-          <input
-            type="password"
-            onChange={(evt) => setPassword(evt.target.value)}
-          />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-          </div>
-      </form>
-    </div>
-  );
+    return(
+        <div className='login-wrapper'>
+            <h1>Please login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Username</p>
+                    <input type='text' onChange={evt=>setUserName(evt.target.value)}/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type='password'onChange={evt=>setPassword(evt.target.value)}/>
+                </label>
+                <div>
+                    <button type='submit'>Submit</button>
+                </div>
+            </form>
+        </div>
+    );
 }
+
 export default Login;
-// import React, {useState} from 'react';
-
-// async function fetchToken(creds)
-// {
-//     return fetch('http://localhost:8080/login', {
-//         method: 'POST',
-//         header:{
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(creds)
-//     }).then(data => data.json());
-// }
-
-// function Login( {setToken} )
-// {
-//     const [username, setUserName] =useState();
-//     const [password, setPassword] = useState();
-
-//     const handleSubmit = async evt => {
-//         evt.preventDefault();
-//         const token = await fetchToken({
-//             username, 
-//             password,
-//         });
-//         setToken(token);
-//     }
-
-//     return(
-//         <div className='login-wrapper'>
-//             <h1>Please login</h1>
-//             <form onSubmit={handleSubmit}>
-//                 <label>
-//                     <p>Username</p>
-//                     <input type='text' onChange={evt=>setUserName(evt.target.value)}/>
-//                 </label>
-//                 <label>
-//                     <p>Password</p>
-//                     <input type='password'onChange={evt=>setPassword(evt.target.value)}/>
-//                 </label>
-//                 <div>
-//                     <button type='submit'>Submit</button>
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// }
-
-// export default Login;
