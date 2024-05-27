2f76596742671de30c88fd0c43ef848cbe4c5698
anthonyhenry <anthonyh1497@gmail.com>
2024-05-27 13:36:25 -0700

    express server setup

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/App/index.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Login from '../Login'
 import LandingPage from '../Landing'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
@@ -7,6 +7,13 @@ import * as ROUTES from "../constants/paths"
 
 function App()
 {
+    const [token, setToken] = useState();
+
+    if(!token)
+    {
+        return <Login setToken={setToken}/>   
+    }
+
     return(
         <>
             <h1>App</h1>
