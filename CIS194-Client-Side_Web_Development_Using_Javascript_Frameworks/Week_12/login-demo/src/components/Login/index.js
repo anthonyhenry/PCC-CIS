6b3567cf7e6c9893eb01710bbb81148e65419b24
anthonyhenry <anthonyh1497@gmail.com>
2024-06-01 14:31:04 -0700

    Update index.js

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/Login/index.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_12/login-demo/src/components/Login/index.js
@@ -44,9 +44,16 @@ function Login( {setToken} )
                     <input 
                         type='text' 
                         onChange={evt=>setUserName(evt.target.value)}
+                        onFocus={()=>setError(true)}
+                        onBlur={()=>setError(false)}
                     />
                 </label>
-                {error && <div>You entered an invalid character: *</div>}
+                {error && 
+                    <div>
+                        <span>✅: All alphanumeric characters</span>
+                        <br/>
+                        <span>⛔: *</span>
+                    </div>}
                 <label>
                     <p>Password</p>
                     <input 
