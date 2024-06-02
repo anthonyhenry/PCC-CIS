d2861764e955268929c21aa1f6f96b64d7e60583
anthonyhenry <anthonyh1497@gmail.com>
2024-06-01 18:31:10 -0700

    Setup and passing data through props

--- CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_14/hw12_1_tic-tac-toe/src/App.js
+++ CIS194-Client-Side_Web_Development_Using_Javascript_Frameworks/Week_14/hw12_1_tic-tac-toe/src/App.js
@@ -1,3 +1,29 @@
-export default function Square() {
-  return <button className="square">X</button>;
+// Components must start with a capital letter
+function Square( {value} )
+{
+  return <button className="square">{value}</button>; 
+}
+
+// The export JavaScript keyword makes this function accessible outside of this file
+// The default keyword tells other files using your code that it’s the main function in your file
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="X"/>
+        <Square value="X"/>
+        <Square value="X"/>
+      </div>
+      <div className="board-row">
+        <Square value="X"/>
+        <Square value="X"/>
+        <Square value="X"/>
+      </div>
+      <div className="board-row">
+        <Square value="X"/>
+        <Square value="X"/>
+        <Square value="X"/>
+      </div>
+    </>
+  )
 }
